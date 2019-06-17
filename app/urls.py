8d48742dd7f32a3d57e841605a4e626405839ba4
kailash <ahikailash1@gmail.com>
2019-06-17 21:28:50 +0530

    Interest screen updated and home screen added

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^home/', views.home, name='home'),
-    url(r'^interests/', views.get_interests, name='get_interests'),
+    url(r'^$', views.index, name='index'),
+    url(r'^home$', views.home, name='home'),
+    url(r'^interests$', views.get_interests, name='get_interests'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,14 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django_tables2 import RequestConfig
 
 from app.forms import InterestForm
 from app.models import Interest
 from app.tables import InterestTable
+
+
+def index(request):
+    return render(request, "index.html")
 
 
 def home(request):
@@ -17,11 +21,17 @@ def get_interests(request):
         form = InterestForm(request.POST)
         # Check the validity of the form
         if form.is_valid():
-            interest = form.save(commit=False)
-            interest.save()
-            return HttpResponseRedirect('/user/interests/')
-        
-        return render(request, 'interest.html', {'form': form})
+            name = form.cleaned_data['name']
+            if not Interest.objects.filter(name=name).exists():
+                interest = form.save(commit=False)
+                interest.save()
+                return HttpResponseRedirect('/user/interests/')
+            else:
+                form.add_error(field='name', error="Duplicated value")
+
+        interests = InterestTable(Interest.objects.all())
+        RequestConfig(request, paginate={'per_page': 25}).configure(interests)
+        return render(request, 'interest.html', {'form': form, 'interests': interests})
     else:
         interests = InterestTable(Interest.objects.all())
         RequestConfig(request, paginate={'per_page': 25}).configure(interests)
