76e7de3ed058d64abb91675206ca95a5ada4c623
kailash <ahikailash1@gmail.com>
2019-07-03 01:57:12 +0530

    Unique user id generation added

--- userweb/wsgi.py
+++ userweb/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+import secrets
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'userweb.settings')
+
+print("User id:", secrets.token_urlsafe(20))
 
 application = get_wsgi_application()
